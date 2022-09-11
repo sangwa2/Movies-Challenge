fb30cadf50c256e6c5d1b8f5416cf1a000d114cb
SANGWA <88146487+sangwa2@users.noreply.github.com>
2022-09-11 03:37:07 +0200

    Add files via upload

---
+++ MoviesApi/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Data;
+using MoviesAppModels.Models;
+
+namespace MoviesApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase {
+
+        private readonly DataContext bookingContext;
+        public BookingController(DataContext context) {
+            bookingContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bookings>>>  Index() {
+            return await bookingContext.Bookings.ToListAsync();
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Bookings>> AddBooking(Bookings bookings) {
+            bookingContext.Bookings.Add(bookings);
+            return RedirectToAction("Index");
+            bookingContext.SaveChanges();
+           // return CreatedAtAction("GetBooking", new { id = bookings.id }, bookings);
+        }
+
+
+    }
+}
